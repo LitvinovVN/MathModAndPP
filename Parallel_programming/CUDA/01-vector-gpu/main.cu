dd4ab14b3db349ac9468cfb61bc5f73cb790d3e7
Vladimir Litvinov <litvinovvn@rambler.ru>
2024-11-05 18:39:58 +0300

    05.11.2024_18:39:58.06

--- Parallel_programming/CUDA/01-vector-gpu/main.cu
+++ Parallel_programming/CUDA/01-vector-gpu/main.cu
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <string>
 #include "GpuLib/_include.cu"
 
 // Структура для хранения параметров запуска теста производительности
@@ -31,6 +33,21 @@ struct TestConfig
     unsigned iterNum = 10;    
 };
 
+bool compare(const FuncResultScalar<double>& left, const FuncResultScalar<double>& right) 
+{ 
+    return left.Time_mks < right.Time_mks; 
+}
+
+void print(std::string message, std::vector<FuncResultScalar<double>> results)
+{
+    std::cout << message << std::endl;
+    for(auto& item : results)
+    {
+        item.Print();
+        //std::cout << std::endl;
+    }
+}
+
 // Статистические параметры результатов эксперимента
 struct CalculationStatistics
 {
@@ -44,14 +61,77 @@ struct CalculationStatistics
     double avg;
     // Медиана
     double median;
-    // 
-};
+    // 95 перецентиль
+    double percentile_95;
+    // Среднеквадратическое отклонение
+    double stdDev;
 
-CalculationStatistics CalculateStatistics(std::vector<FuncResultScalar<double>> results)
-{
-    CalculationStatistics stat;
-    return stat;
-}
+    CalculationStatistics(std::vector<FuncResultScalar<double>> results)
+    {
+        auto resultsSize = results.size();
+        if (resultsSize == 0)
+            throw std::logic_error("results size is 0");
+
+        // Проверяем корректность результатов        
+        for(unsigned i = 1; i < resultsSize; i++)
+        {
+            if(results[i].Status == false)
+                throw std::logic_error("results[i].Status = 0");
+            
+            if( fabs((results[i].Result - results[0].Result) / results[0].Result) > 0.0001 )
+                throw std::logic_error("fabs((results[i].Result - results[0].Result) / results[0].Result) > 0.0001");
+        }
+
+        print(std::string("---Before sort---"), results);
+        // Сортируем results
+        std::sort(results.begin(), results.end(), compare);
+        print(std::string("---After sort---"), results);        
+        std::cout << "----------" << std::endl;
+
+        minValue = results[0].Time_mks;
+        maxValue = results[resultsSize - 1].Time_mks;
+
+        if(resultsSize % 2 == 0)
+        {
+            median = (results[resultsSize / 2 - 1].Time_mks + results[resultsSize / 2].Time_mks)/2;
+        }
+        else
+        {
+            median = results[resultsSize / 2].Time_mks;
+        }
+
+        // Вычисляем среднее арифметическое
+        double sum = 0;
+        for(auto& item : results)
+            sum += item.Time_mks;
+        
+        avg = sum / resultsSize;
+
+        // Вычисляем стандартное отклонение
+        double sumSq = 0;
+        for(auto& item : results)
+            sumSq += pow(item.Time_mks - avg, 2);
+        
+        stdDev = sqrt(sumSq / resultsSize);
+
+        // Вычисляем 95 перцентиль
+        unsigned ind95 = resultsSize;
+        percentile_95 = -95.959595;// Доделать
+
+        Print();
+    }
+
+    void Print()
+    {
+        std::cout   << "minValue: "      << minValue << "; "
+                    << "median: "        << median   << "; "
+                    << "avg: "           << avg      << "; "
+                    << "percentile_95: " << percentile_95   << "; "
+                    << "maxValue: "      << maxValue << "; "                                                            
+                    << "stdDev: "        << stdDev   << "; "
+                    << std::endl;
+    }
+};
 
 
 // Выполняет набор тестов производительности для VectorGpu::Sum
@@ -98,7 +178,7 @@ void StartTestVectorGpuSum(TestConfig conf)
                     results[iter] = res;
                     //std::cout << results[iter].Status << " | " << results[iter].Result << " | " << results[iter].Time_mks << std::endl;
                 }
-                CalculationStatistics stat = CalculateStatistics(results);
+                CalculationStatistics stat(results);
 
                 out << blocksNum << "; " << threadsNum << "; " << results[0].Status << "; " << results[0].Result << "; "<< results[0].Time_mks << std::endl;
             }
