9ea53e3f3039be2c533150d943af991760e502b2
litvinovvn <litvinovvn@rambler.ru>
2024-11-11 13:05:35 +0300

    11.11.2024_13:05:35.43

--- Parallel_programming/CUDA/01_HelloGPU_compilation/compile.bat
+++ Parallel_programming/CUDA/01_HelloGPU_compilation/compile.bat
@@ -1,8 +1,6 @@
-echo "!!!!!NOT WORKING!!!!"
-
 echo "Compiling..."
-nvcc -O3 kernel.cu -dc -o target.o -gencode arch=compute_52,code=sm_52
-nvcc -O3 target.o -o dlink.o -gencode arch=compute_52,code=sm_52 -dlink
+nvcc -O3 kernel.cu -dc -o target.o -gencode arch=compute_75,code=sm_75
+nvcc -O3 target.o -o dlink.o -gencode arch=compute_75,code=sm_75 -dlink
 nvcc -c main.cpp -o main.o
 nvcc dlink.o main.o target.o -o app
 echo "Starting app.exe..."
