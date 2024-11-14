91813301b984a1bdd879267e8c332df39c6cdc0f
Vladimir Litvinov <litvinovvn@rambler.ru>
2024-11-14 17:54:01 +0300

    14.11.2024_17:54:01.88

--- Parallel_programming/C++/11_template_class_func/main.cpp
+++ Parallel_programming/C++/11_template_class_func/main.cpp
@@ -25,12 +25,14 @@ struct FuncResult
     }
 };
 
+
 template<typename T>
+inline
 void thread_sum(T* data, size_t indStart, size_t indEnd, T& sum)
 {
     T local_sum = 0;
 
-    for (size_t i = indStart; i < indEnd; i++)
+    for (size_t i = indStart; i <= indEnd; i++)
     {
         local_sum += data[i];
     }
@@ -77,30 +79,36 @@ public:
         std::cout << std::endl;     
     }
 
-    T Sum()
+    T Sum(size_t indStart, size_t indEnd)
     {
         T result = 0;
-        for (size_t i = 0; i < size; i++)
+        for (size_t i = indStart; i <= indEnd; i++)
         {
             result += data[i];
         }
         return result;
     }
 
-    T Sum(unsigned threadsNum)
+    T Sum()
+    {
+        T result = Sum(0, size-1);
+        return result;
+    }
+
+    T Sum(size_t indStart, size_t indEnd, unsigned threadsNum)
     {
         T sum = 0;
-
+        size_t blockSize = indEnd - indStart + 1;
         std::vector<std::thread> threads;
-        size_t blockSize = size / threadsNum;
+        size_t thBlockSize = blockSize / threadsNum;
         for (size_t i = 0; i < threadsNum; i++)
         {            
-            size_t indStart = i * blockSize;
-            size_t indEnd = indStart + blockSize;
+            size_t thIndStart = i * thBlockSize;
+            size_t thIndEnd = thIndStart + thBlockSize - 1;
             if(i == threadsNum - 1)
-                indEnd = size;
+                thIndEnd = indEnd;
 
-            threads.push_back(std::thread(thread_sum<T>, data, indStart, indEnd, std::ref(sum)));
+            threads.push_back(std::thread(thread_sum<T>, data, thIndStart, thIndEnd, std::ref(sum)));
         }
         
         for(auto& th : threads)
@@ -111,22 +119,60 @@ public:
         return sum;
     }
 
-    FuncResult<T> Sum2()
+    T Sum(unsigned threadsNum)
+    {
+        return Sum(0, size - 1, threadsNum);
+        /*T sum = 0;
+
+        std::vector<std::thread> threads;
+        size_t blockSize = size / threadsNum;
+        for (size_t i = 0; i < threadsNum; i++)
+        {            
+            size_t indStart = i * blockSize;
+            size_t indEnd = indStart + blockSize - 1;
+            if(i == threadsNum - 1)
+                indEnd = size - 1;
+
+            threads.push_back(std::thread(thread_sum<T>, data, indStart, indEnd, std::ref(sum)));
+        }
+        
+        for(auto& th : threads)
+        {
+            th.join();
+        }
+
+        return sum;*/
+    }
+
+    FuncResult<T> SumFR(size_t indStart, size_t indEnd)
     {
         auto start = high_resolution_clock::now();
+        T result = Sum(indStart, indEnd);
+        auto stop = high_resolution_clock::now();
+
+        auto duration = duration_cast<microseconds>(stop - start);        
+        auto t = duration.count();
+
+        return FuncResult<T>(result, t);
+    }
+
+    FuncResult<T> SumFR()
+    {
+        return SumFR(0, size - 1);
+        /*auto start = high_resolution_clock::now();
         T result = Sum();
         auto stop = high_resolution_clock::now();
 
         auto duration = duration_cast<microseconds>(stop - start);        
         auto t = duration.count();
 
-        return FuncResult<T>(result, t);
+        return FuncResult<T>(result, t);*/
     }
 
-    FuncResult<T> Sum2(unsigned threadsNum)
+    FuncResult<T> SumFR(size_t indStart, size_t indEnd, unsigned threadsNum)
     {
         auto start = high_resolution_clock::now();
-        T result = Sum(threadsNum);
+        T result = Sum(indStart, indEnd, threadsNum);
         auto stop = high_resolution_clock::now();
 
         auto duration = duration_cast<microseconds>(stop - start);        
@@ -134,11 +180,25 @@ public:
 
         return FuncResult<T>(result, t);
     }
+
+    FuncResult<T> SumFR(unsigned threadsNum)
+    {
+        return SumFR(0, size, threadsNum);
+        /*auto start = high_resolution_clock::now();
+        T result = Sum(threadsNum);
+        auto stop = high_resolution_clock::now();
+
+        auto duration = duration_cast<microseconds>(stop - start);        
+        auto t = duration.count();
+
+        return FuncResult<T>(result, t);*/
+    }
 };
 
 int main()
 {
-    size_t size = 1000000;
+    unsigned Nthreads = 4;
+    size_t size = 100000000;
     double elVal = 0.001;
     Vec<double> v(size);
     v.InitByVal(elVal);
@@ -148,12 +208,42 @@ int main()
     auto sum_seq = v.Sum();
     std::cout << "sum_seq = " << sum_seq << std::endl;
 
-    auto sum_par = v.Sum(4);
+    std::cout << "sum_seq_half must be equal " << (size / 2) * elVal << std::endl;
+    auto sum_seq_half = v.Sum(0, size / 2);
+    std::cout << "sum_seq_half = " << sum_seq_half << std::endl;
+    ///////////////////////////////////////////////////
+
+    auto sum_par = v.Sum(Nthreads);
     std::cout << "sum_par = " << sum_par << std::endl;
 
-    auto sum2 = v.Sum2();
-    sum2.Print();
+    auto sum_par_half = v.Sum(0, size / 2, Nthreads);
+    std::cout << "sum_par_half = " << sum_par_half << std::endl;
+    ///////////////////////////////////////////////////
 
-    auto sum2_par = v.Sum2(4);
-    sum2_par.Print();
+    auto sumFR = v.SumFR();
+    std::cout << "sumFR: ";
+    sumFR.Print();
+
+    auto sumFR_half = v.SumFR(0, size / 2);
+    std::cout << "sumFR_half: ";
+    sumFR_half.Print();
+    ///////////////////////////////////////////////////
+
+    auto sumFR_par = v.SumFR(Nthreads);
+    std::cout << "sumFR_par: ";
+    sumFR_par.Print();
+
+    auto sumFR_par_half = v.SumFR(0, size / 2, Nthreads);
+    std::cout << "sumFR_par_half: ";
+    sumFR_par_half.Print();
+    ///////////////////////////////////////////////////
+
+    auto S = (double)sumFR._time / sumFR_par._time;
+    std::cout << "S = " << S << std::endl;
+
+    ///////////////////////////////////////////////////
+
+    auto E = S / Nthreads;
+    std::cout << "E = " << E << std::endl;
+    ///////////////////////////////////////////////////
 }
