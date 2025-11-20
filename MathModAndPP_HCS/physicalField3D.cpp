083d576680029f0bf6833be22fa0250c46e836ac
LitvinovVN <LitvinovVN@rambler.ru>
2025-11-20 20:57:24 +0300

    20.11.2025_20:57:24.10

---
+++ MathModAndPP_HCS/physicalField3D.cpp
@@ -0,0 +1,12 @@
+// Трёхмерное поле физической величины
+// g++ physicalField3D.cpp -o physicalField3D
+// ./physicalField3D
+#include <iostream>
+#include "HCSLib/_IncludeLib.hpp"
+
+int main()
+{
+    std::cout << "--- Physical Field 3D ---\n";
+
+    
+}
